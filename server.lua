f08504e869e2a79974b66dedf0bbd5afa6fca402
Ku1an <46690818+Ku1an@users.noreply.github.com>
2020-04-07 00:40:54 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,3 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-
 RegisterNetEvent('esx:playerJoined')
 AddEventHandler('esx:playerJoined', function()
     local playerid = source
